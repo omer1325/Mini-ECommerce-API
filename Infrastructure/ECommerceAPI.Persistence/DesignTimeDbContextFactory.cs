324a22066ce70eca01d96c946e88886de3d5599a
Ömer Faruk İngeç <49835381+omer1325@users.noreply.github.com>
2023-02-18 13:38:37 +0300

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace ECommerceAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceAPIDbContext>
+    {
+        public ECommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ECommerceAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new (dbContextOptionsBuilder.Options);
+        }
+    }
+}
