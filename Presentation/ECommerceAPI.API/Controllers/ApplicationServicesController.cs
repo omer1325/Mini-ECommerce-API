2124a72e32367a9ff1538ac07e1c563cfddc422d
omer1325 <omer.ingec24@gmail.com>
2023-08-10 00:03:05 +0300

    Creating the Role Management Page

--- Presentation/ECommerceAPI.API/Controllers/ApplicationServicesController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ApplicationServicesController.cs
@@ -1,11 +1,14 @@
 ﻿using ECommerceAPI.Application.Abstractions.Services.Configurations;
-using Microsoft.AspNetCore.Http;
+using ECommerceAPI.Application.CustomAttributes;
+using ECommerceAPI.Application.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = "Admin")]
     public class ApplicationServicesController : ControllerBase
     {
         readonly IApplicationService _applicationService;
@@ -16,6 +19,7 @@ namespace ECommerceAPI.API.Controllers
         }
 
         [HttpGet]
+        [AuthorizeDefinition(ActionType = ActionType.Reading, Definition = "Get Authorize Definition Endpoints", Menu = "Application Services")]
         public IActionResult GetAuthorizeDefinitionEndpoints()
         {
             var datas = _applicationService.GetAuthorizeDefinitionEndpoints(typeof(Program));
