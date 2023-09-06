1e0f2b4580fb820ed69f2440ba01e7ff0cba373b
omer1325 <omer.ingec24@gmail.com>
2023-09-06 15:50:46 +0300

    Creating the User Page and User Role Relationship

---
+++ Core/ECommerceAPI.Application/Features/Queries/AppUser/GetRolesToUser/GetRolesToUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceAPI.Application.Abstractions.Services;
+using MediatR;
+
+namespace ECommerceAPI.Application.Features.Queries.AppUser.GetRolesToUser
+{
+    public class GetRolesToUserQueryHandler : IRequestHandler<GetRolesToUserQueryRequest, GetRolesToUserQueryResponse>
+    {
+        readonly IUserService _userService;
+
+        public GetRolesToUserQueryHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<GetRolesToUserQueryResponse> Handle(GetRolesToUserQueryRequest request, CancellationToken cancellationToken)
+        {
+            var userRoles = await _userService.GetRolesToUserAsync(request.UserId);
+            return new()
+            {
+                UserRoles = userRoles
+            };
+        }
+    }
+}
